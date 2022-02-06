bfa7c73fd8b66baac0ab5b159226e5bbbdfc5abe
Katalyst Kat <96984912+katalystkat@users.noreply.github.com>
2022-02-06 21:12:06 +0800

    Add files via upload

---
+++ day26_ListComprehensionandNATOAlphabet/Lesson_main.py
@@ -0,0 +1,21 @@
+student_dict = {
+    "student":["Angela", "James", "Lily"],
+    "score" : [56, 76, 98]
+}
+
+#Looping
+# for (key, value) in student_dict.items():\
+# print(value)
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+#Loop through a data frame
+#for (key, value) in student_data_frame.items():
+ #   print(key)
+
+#Loop through rows of a dataframe
+for (index, row) in student_data_frame.iterrows():
+    if row.student =='Angela':
+        print(row.score)
